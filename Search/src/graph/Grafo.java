965ebd4c74b58a533a9dd54e61ed73a59f5c23f7
Adolfo Guimarães <adolfoguimaraes@gmail.com>
2018-03-20 18:14:06 -0300

    Segunda Tarefa - Algoritmos de Busca

---
+++ Search/src/graph/Grafo.java
@@ -0,0 +1,75 @@
+package graph;
+
+import graph.interfaces.IGrafo;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Grafo implements IGrafo {
+
+    private Set<Vertice> vertices = new HashSet<Vertice>();
+    private Set<Aresta> arestas = new HashSet<Aresta>();
+
+    public Grafo(Set<Vertice> vertices, Set<Aresta> arestas) {
+        this.vertices = vertices;
+        this.arestas = arestas;
+    }
+
+    @Override
+    public Set<Vertice> getAdjacentes(Vertice v) {
+        Set<Vertice> adjacentes = new HashSet<Vertice>();
+
+        for(Aresta aresta: this.arestas) {
+            if(aresta.getA() == v) {
+                adjacentes.add(aresta.getB());
+            }
+
+            if(aresta.getB() == v) {
+                adjacentes.add(aresta.getA());
+            }
+        }
+        return adjacentes;
+
+    }
+
+    @Override
+    public boolean ehConexo() {
+        return false;
+    }
+
+    @Override
+    public void buscaLargura() {
+
+    }
+
+    @Override
+    public void buscaProfundidade() {
+
+    }
+
+
+    @Override
+    public String toString() {
+
+        String retorno = "";
+
+        for(Vertice v: vertices) {
+            retorno += v.toString() + "[ ";
+
+            Set<Vertice> adjacentes = this.getAdjacentes(v);
+
+            for(Vertice adj: adjacentes) {
+                retorno += adj.toString() + " ";
+            }
+
+            retorno += "]\n";
+
+
+        }
+
+        return retorno;
+    }
+
+
+}
